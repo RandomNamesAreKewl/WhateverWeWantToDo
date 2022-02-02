66569cca9450c348099988fa5b8020100464c85a
Owenknowsbest <78163015+Owenknowsbest@users.noreply.github.com>
2022-02-02 16:27:17 -0700

    Initial commit

---
+++ src/main/java/randname/arm/Arm.java
@@ -0,0 +1,9 @@
+package randname.arm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Arm implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/randname/arm/client/ArmClient.java
@@ -0,0 +1,10 @@
+package randname.arm.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class ArmClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
